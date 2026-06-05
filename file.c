fc91a60c35e2238b90d4a486f5d9c00e85113cca
pavel-2009 <ms.radionova@bk.ru>
2026-06-05 15:07:42 +0500

    first project && started c++

--- file.c
+++ file.c
@@ -1,47 +1,111 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 
-int main(int argc, char *argv[]) {
+struct Task {
+    int id;
+    char text[100];
+    struct Task *next;
+};
 
-    if (argc < 2) {
-        printf("Ошибка: Передайте число при запуске программы.\n");
-        return 1;
+
+struct Task* add_task(struct Task* head, int id, char *text) {
+
+    struct Task* new_task = (struct Task*) malloc(sizeof(struct Task));
+    if (new_task == NULL) {
+        printf("Ошибка при выделении памяти.");
+        return NULL;
     };
 
-    int age = atoi(argv[1]);
+    new_task->id = id;
+    strncpy(new_task->text, text, sizeof(new_task->text) - 1);
+    new_task->text[sizeof(new_task->text) - 1] = '\0';
+    new_task->next = head;
 
-    // Запись в файл
-    FILE *file = fopen("age.txt", "w");
+    return new_task;
+}
+
+
+void save_to_file(struct Task *head, char *filename) {
+
+    FILE *file = fopen(filename, "w");
 
     if (file == NULL) {
-        printf("Ошибка: Не удалось создать или открыть файл для записи!\n");
-        return 1;
+        printf("Не удалось открыть файл\n");
+        return;
     };
 
-    fprintf(file, "Age: %d\n", age);
+    struct Task* current = head;
+
+    while (current != NULL) {
+        fprintf(file, "%d %s\n", current->id, current->text);
+        current = current->next;
+    };
+
+    fclose(file);
+    file = NULL;
+}
+
+
+struct Task* load_from_file(char *filename) {
+    struct Task *head = NULL;
+
+    FILE *file = fopen(filename, "r");
+    if (file == NULL) return NULL;
+
+    int id;
+    char text[100];
+
+    while (fscanf(file, "%d %s", &id, text) == 2) {
+        head = add_task(head, id, text);
+    };
 
     fclose(file);
     file = NULL;
 
-    // Чтение из файла
-    FILE *file_in = fopen("age.txt", "r");
+    return head;
+}
 
-    if (file_in == NULL) {
-        printf("Ошибка: Файл не найден для чтения!\n");
-        return 1;
+
+int main(int argc, char *args[]) {
+
+    system("chcp 65001"); 
+
+    char *filename = "tasks.txt";
+
+    struct Task *head = load_from_file(filename);
+
+    if (argc >= 2) {
+        int new_id = 1;
+        if (head != NULL) {
+            new_id = head->id + 1;
+        };
+
+        head = add_task(head, new_id, args[1]);
+        printf("Добавлена задача: [%d] %s\n", new_id, args[1]);
     };
 
-    char label[20];
-    int parsed_age;
+    printf("\n=== ТВОЙ СПИСОК ЗАДАЧ ===\n");
 
-    fscanf(file_in, "%s %d", label, &parsed_age);
+    struct Task *current = head;
+    if (current == NULL) printf("Задач пока нет. Добавь: %s \"Текст_задачи\"\n", args[0]);
 
-    fclose(file_in);
-    file_in = NULL;
+    while (current != NULL) {
+        printf("[%d] %s\n", current->id, current->text);
+        current = current->next;
+    };
 
-    printf("Данные успешно прочитаны из файла!\n");
-    printf("Метка: %s, Значение: %d\n", label, parsed_age);
+    save_to_file(head, filename);
 
-    return 1;
+    current = head;
+    while (current != NULL) {
+        struct Task *next_task = current->next;
+        free(current);
+        current = next_task;
+    };
+    head = NULL;
+
+    return 0;
+
 }

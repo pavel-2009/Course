1a1448caf338b7a8d05af2cf0d0873df21f5f821
pavel-2009 <ms.radionova@bk.ru>
2026-06-06 14:20:28 +0500

    first block of C++ completed

---
+++ text_analyser/build/CMakeFiles/text_stat.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/text_stat.dir/analyzer.cpp.obj"
+  "CMakeFiles/text_stat.dir/analyzer.cpp.obj.d"
+  "CMakeFiles/text_stat.dir/main.cpp.obj"
+  "CMakeFiles/text_stat.dir/main.cpp.obj.d"
+  "libtext_stat.dll.a"
+  "text_stat.exe"
+  "text_stat.exe.manifest"
+  "text_stat.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/text_stat.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

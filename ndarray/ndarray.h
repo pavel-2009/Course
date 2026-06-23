ff6e8d2acf1f2fe07d23027f6bfd92f7bec5e571
pavel-2009 <ms.radionova@bk.ru>
2026-06-23 10:16:18 +0500

    updated ndarray code

--- ndarray/ndarray.h
+++ ndarray/ndarray.h
@@ -16,103 +16,31 @@ class NDArray {
         std::vector<size_t> strides_;
 
         // Вспомогательная функция для вычисления strides
-        std::vector<size_t> compute_strides(const std::vector<size_t>& shape) {
-            std::vector<size_t> strides(shape.size());
-            size_t stride = 1;
-            for (int i = static_cast<int>(shape.size()) - 1; i >= 0; --i) {
-                strides[i] = stride;
-                stride *= shape[i];
-            }
-            return strides;
-        }
+        std::vector<size_t> compute_strides(const std::vector<size_t>& shape) {}
 
     public:
-        NDArray(const std::vector<size_t>& shape) : shape_(shape) {
-            size_t total_size = 1;
-            for (size_t dim : shape) {
-                total_size *= dim;
-            }
-            data_.resize(total_size, T());
-            strides_ = compute_strides(shape);
-        }
+        NDArray(const std::vector<size_t>& shape) : shape_(shape) {}
 
         // Возвращает сырой указатель на данные (non-const версия)
-        T* data() {
-            return data_.data();
-        }
+        T* data() {}
 
         // Возвращает const указатель
-        const T* data() const {
-            return data_.data();
-        }
+        const T* data() const {}
 
-        const std::vector<size_t>& shape() const {
-            return shape_;
-        }
+        const std::vector<size_t>& shape() const {}
 
-        const std::vector<size_t>& strides() const {
-            return strides_;
-        }
+        const std::vector<size_t>& strides() const {}
 
-        const size_t size() const {
-            return data_.size();
-        }
+        const size_t size() const {}
 
-        void fill(const T& value) {
-            std::fill(data_.begin(), data_.end(), value);
-        }
+        void fill(const T& value) {}
 
         // Оператор доступа для 1D, 2D, 3D... индексов
         template<typename... Args>
-        T& operator()(Args... indices) {
-            static_assert((std::is_convertible_v<Args, size_t> && ...), "All indices must be convertible to size_t.");
-
-            std::array<size_t, sizeof...(Args)> idx{static_cast<size_t>(indices)...};
-            
-            // Проверка количества индексов
-            if (idx.size() != shape_.size()) {
-                throw std::invalid_argument("Number of indices doesn't match array dimensions");
-            }
-
-            // Проверка границ
-            for (size_t i = 0; i < idx.size(); ++i) {
-                if (idx[i] >= shape_[i]) {
-                    throw std::out_of_range("Index out of bounds");
-                }
-            }
-
-            // Вычисление смещения
-            size_t offset = 0;
-            for (size_t i = 0; i < idx.size(); ++i) {
-                offset += idx[i] * strides_[i];
-            }
-
-            return data_[offset];
-        }
+        T& operator()(Args... indices) {}
 
         template<typename... Args>
-        const T& operator()(Args... indices) const {
-            static_assert((std::is_convertible_v<Args, size_t> && ...), "All indices must be convertible to size_t.");
-
-            std::array<size_t, sizeof...(Args)> idx{static_cast<size_t>(indices)...};
-            
-            if (idx.size() != shape_.size()) {
-                throw std::invalid_argument("Number of indices doesn't match array dimensions");
-            }
-
-            for (size_t i = 0; i < idx.size(); ++i) {
-                if (idx[i] >= shape_[i]) {
-                    throw std::out_of_range("Index out of bounds");
-                }
-            }
-
-            size_t offset = 0;
-            for (size_t i = 0; i < idx.size(); ++i) {
-                offset += idx[i] * strides_[i];
-            }
-
-            return data_[offset];
-        }
+        const T& operator()(Args... indices) const {}
 };
 
 #endif // NDARRAY_H
--- ndarray/ndview.h
+++ ndarray/ndview.h
@@ -17,85 +17,23 @@ class NDView {
         NDView(T* data, const std::vector<size_t>& shape, const std::vector<size_t>& strides)
             : data_(data), shape_(shape), strides_(strides) {}
 
-        T* data() {
-            return data_;
-        }
+        T* data() {}
 
-        const T* data() const {
-            return data_;
-        }
+        const T* data() const { }
 
-        const std::vector<size_t>& shape() const {
-            return shape_;
-        }
+        const std::vector<size_t>& shape() const {}
 
-        const std::vector<size_t>& strides() const {
-            return strides_;
-        }
+        const std::vector<size_t>& strides() const {}
 
         // Оператор доступа для переменного числа индексов
         template<typename... Args>
-        T& operator()(Args... indices) {
-            static_assert((std::is_convertible_v<Args, size_t> && ...), "All indices must be convertible to size_t.");
-
-            std::array<size_t, sizeof...(Args)> idx{static_cast<size_t>(indices)...};
-            
-            if (idx.size() != shape_.size()) {
-                throw std::invalid_argument("Number of indices doesn't match view dimensions");
-            }
-
-            // Проверка границ
-            for (size_t i = 0; i < idx.size(); ++i) {
-                if (idx[i] >= shape_[i]) {
-                    throw std::out_of_range("Index out of bounds");
-                }
-            }
-
-            // Вычисление смещения
-            size_t offset = 0;
-            for (size_t i = 0; i < idx.size(); ++i) {
-                offset += idx[i] * strides_[i];
-            }
-
-            return data_[offset];
-        }
+        T& operator()(Args... indices) {}
 
         template<typename... Args>
-        const T& operator()(Args... indices) const {
-            static_assert((std::is_convertible_v<Args, size_t> && ...), "All indices must be convertible to size_t.");
-
-            std::array<size_t, sizeof...(Args)> idx{static_cast<size_t>(indices)...};
-            
-            if (idx.size() != shape_.size()) {
-                throw std::invalid_argument("Number of indices doesn't match view dimensions");
-            }
-
-            for (size_t i = 0; i < idx.size(); ++i) {
-                if (idx[i] >= shape_[i]) {
-                    throw std::out_of_range("Index out of bounds");
-                }
-            }
-
-            size_t offset = 0;
-            for (size_t i = 0; i < idx.size(); ++i) {
-                offset += idx[i] * strides_[i];
-            }
-
-            return data_[offset];
-        }
+        const T& operator()(Args... indices) const {}
 
         // Метод для получения view строки
-        NDView<T> row(size_t row_index) {
-            if (row_index >= shape_[0]) {
-                throw std::out_of_range("Row index out of bounds");
-            }
-
-            std::vector<size_t> new_shape(shape_.begin() + 1, shape_.end());
-            std::vector<size_t> new_strides(strides_.begin() + 1, strides_.end());
-
-            T* row_data = data_ + row_index * strides_[0];
-            return NDView<T>(row_data, new_shape, new_strides);
-        }
+        NDView<T> row(size_t row_index) {}
 };
 
 #endif // NDVIEW_H

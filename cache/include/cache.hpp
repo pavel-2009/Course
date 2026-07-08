72c31a3cd40bd49fb86b82a63e30189d39ee1590
pavel-2009 <ms.radionova@bk.ru>
2026-07-08 12:22:56 +0500

    created basic lru cache

--- cache/include/cache.hpp
+++ cache/include/cache.hpp
@@ -3,6 +3,7 @@
 #include <list>
 #include <unordered_map>
 #include <string>
+#include <stdexcept>
 
 
 template<typename T, typename U>
@@ -10,7 +11,7 @@ class Cache {
     private:
         size_t capacity;
         std::list<std::pair<T, U>> items;
-        std::unordered_map<T, std::list<std::pair<T, U>>::iterator> item_map;
+        std::unordered_map<T, typename std::list<std::pair<T, U>>::iterator> item_map;
 
     public:
         Cache(size_t capacity) : capacity(capacity) {}
@@ -23,11 +24,63 @@ class Cache {
         Cache(Cache&&) = delete;
         Cache& operator=(Cache&&) = delete;
 
-        U get(const T& key);
-        void put(const T& key, const U& value);
-        bool contains(const T& key) const;
-        void erase(const T& key);
-        void clear();
+        U get(const T& key) {
+
+            auto link = item_map.find(key);
+            if (link == item_map.end()) {
+                throw std::out_of_range("Key not found in cache");
+            }
+
+            // Get the value via the link iterator
+            auto item = link->second;
+            U value = item->second;
+
+            // Move the accessed item to the front of the list
+            items.splice(items.begin(), items, item);
+
+            return value;
+        }
+
+        void put(const T& key, const U& value) {
+    
+            auto link = item_map.find(key);
+            if (link != item_map.end()) {
+                auto item = link->second;
+                item->second = value;
+                items.splice(items.begin(), items, item);
+                return;
+            } else {
+
+                if (items.size() >= capacity) {
+                    auto last_item = items.back();
+                    item_map.erase(last_item.first);
+                    items.pop_back();
+                }
+
+                items.emplace_front(key, value);
+                item_map[key] = items.begin();
+            }
+
+        }
+
+        bool contains(const T& key) const {
+            return item_map.find(key) != item_map.end();
+        }
+
+        void erase(const T& key) {
+            auto link = item_map.find(key);
+            if (link != item_map.end()) {
+                auto item = link->second;
+                items.erase(item);
+                item_map.erase(link);
+            }
+            return;
+        }
+
+        void clear() {
+            items.clear();
+            item_map.clear();
+        }
 
         size_t size() const {
             return items.size();
